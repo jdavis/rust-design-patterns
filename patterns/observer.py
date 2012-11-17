e69dd036331ea6325d3fcb7173d7ac0b8562be32
Josh Davis <joshuadavis42@gmail.com>
2012-11-16 20:25:29 -0600

    Added Observer Pattern template docstring.

---
+++ patterns/observer.py
@@ -0,0 +1,28 @@
+"""
+Observer Pattern
+
+Definition:
+    pass
+
+Also Known As:
+    pass
+
+Problem:
+    pass
+
+Wrong Solution:
+    pass
+
+Correct Solution:
+    pass
+
+Sources:
+    Title: Head First Design Patterns
+    Author(s): Eric Freeman & Elisabeth Freeman
+    Pages: 37-78
+
+    Title: Design Patterns
+    Author(s): Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides
+    Pages: 293-299
+
+"""
