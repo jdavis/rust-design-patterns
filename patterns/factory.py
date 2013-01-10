74ac171ee2904586603a071c7b8f53d8ab630e6e
Josh Davis <josh@joshldavis.com>
2013-01-09 18:49:10 -0600

    Add base factory pattern file

---
+++ patterns/factory.py
@@ -0,0 +1,31 @@
+"""
+Factory Pattern
+
+Definition:
+    pass
+
+Also Known As:
+    pass
+
+Problem:
+    pass
+
+Wrong Solution:
+    pass
+
+Correct Solution:
+    pass
+
+Sources:
+    Title: Head First Design Patterns
+    Author(s): Eric Freeman & Elisabeth Freeman
+    Pages: 109-168
+
+    Title: Design Patterns
+    Author(s): Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides
+    Pages: 107-116
+
+Example Info:
+    pass
+
+"""
