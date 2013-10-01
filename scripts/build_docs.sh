75e899fb606157416f2c9b6a8c3f889b99ccf6c1
Josh Davis <josh@joshldavis.com>
2013-09-30 20:08:20 -0500

    Allow multiple commands to script

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -7,9 +7,51 @@ SITE_DIR="_site/"
 PLUGINS_DIR="$DOCS_DIR/_plugins/"
 LAYOUTS_DIR="$DOCS_DIR/_layouts/"
 
+#
+# Documentation functions
+#
+
+# Prints the basic usage of build_docs.sh.
 function usage() {
     echo Usage: build_docs.sh [serve]
-	exit 1
+}
+
+# Uses Jekyll to serve up the documentation.
+function serve() {
+    echo Serving up documentation...
+    echo
+
+    jekyll serve --source $DOCS_DIR --destination $SITE_DIR
+}
+
+# Checkouts the gh-pages branch, commits the generated documentation and then
+# pushes it.
+function deploy() {
+    echo Deploying documentation...
+    echo
+
+    git checkout $DOC_BRANCH || {
+        echo "Failed to checkout $DOC_BRANCH"
+        exit 1
+    }
+
+    mv $SITE_DIR/* ./
+
+    git add .
+    git commit -m "AUTO: Update generated documentation"
+    git push origin $DOC_BRANCH
+
+    echo
+    echo Deploy finished.
+}
+
+# The default function, it just builds the documentation using Jekyll.
+function build() {
+    echo Building documentation...
+    echo
+
+    # Build documentation
+    echo jekyll build --source $DOCS_DIR --destination $SITE_DIR --plugins $PLUGINS_DIR --layouts $LAYOUTS_DIR
 }
 
 # Do a few sanity checks...
@@ -33,17 +75,21 @@ if [[ $PWD != $GIT_DIR ]]; then
     cd $GIT_DIR
 fi
 
-if [[ $# -eq 1 && $1 == "serve" ]]; then
-    jekyll serve --source $DOCS_DIR --destination $SITE_DIR
-    exit 0
-elif [[ $# -eq 1 && $1 != "serve" ]]; then
-    usage
+if [[ $# -eq 0 ]]; then
+    build
+else
+    # Run functions for the arguments given
+    for cmd in "$@"
+    do
+        case "$cmd" in
+            "build")
+                build;;
+            "deploy")
+                deploy;;
+            "serve")
+                serve;;
+            *)
+                usage;;
+        esac
+    done
 fi
-
-# Build documentation
-jekyll build --source $DOCS_DIR --destination $SITE_DIR --plugins $PLUGINS_DIR --layouts $LAYOUTS_DIR
-
-git checkout $DOC_BRANCH || {
-    echo "Failed to checkout $DOC_BRANCH"
-    exit 1
-}
